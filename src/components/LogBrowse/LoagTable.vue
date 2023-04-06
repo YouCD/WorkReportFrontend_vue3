f52e29f8dfb28a9a29e79237b29dacb73726b388
ycd <ycd>
2023-04-06 12:12:19 +0800

    完成 汉化

--- src/components/LogBrowse/LoagTable.vue
+++ src/components/LogBrowse/LoagTable.vue
@@ -88,23 +88,6 @@ import {type1Store} from "@/store/type1";
 import dayjs, {Dayjs} from "dayjs";
 import {IPageRequestData} from "@/types/commont";
 import {Djs2UnixTime, UnixTime2StrTime} from "@/utils/time";
-import 'moment/dist/locale/zh-cn'
-import moment from "moment";
-moment.locale('zh-cn')
-moment.updateLocale("zh-cn", {
-  months: "一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月".split(
-      "_"
-  ),
-  monthsShort: "1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月".split(
-      "_"
-  ),
-  weekdays: "星期日_星期一_星期二_星期三_星期四_星期五_星期六".split("_"),
-  weekdaysShort: "周日_周一_周二_周三_周四_周五_周六".split("_"),
-  weekdaysMin: "日_一_二_三_四_五_六".split("_"),
-});
-
-
-console.log(moment.weekdaysMin());
 
 
 const columns = [
