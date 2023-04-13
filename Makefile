c9e31a825c67eada3b1c0f93ee763db31ca9655c
ycd <ycd>
2023-04-13 17:50:02 +0800

    fix 日志添加

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 build:
 	npm run build
+	rm -rf  dist/log
 	tar Jcf dist.txz dist
--- src/components/LogBrowse/AddLog.vue
+++ src/components/LogBrowse/AddLog.vue
@@ -1,53 +1,56 @@
 <template>
-  <div class="BoxClass">
-    <div class="LeftClass">
-      <div class="CalendarClass">
-        <a-calendar v-model:value="today" :fullscreen="false" valueFormat="YYYY-MM-DD" @select="onSelect"/>
-      </div>
-      <a-button type="primary" style="width: 100%;margin-top: 8px" @click="goToday">今日</a-button>
-      <a-button type="primary" style="width: 100%;margin-top: 8px" @click="GetWeekLog">本周日志</a-button>
-    </div>
-    <div class="RightClass">
-      <div v-if="!data.showWeekLog">
-        <a-form
-            :model="logData"
-            name="basic"
-            :label-col="{ span: 8 }"
-            :wrapper-col="{ span: 16 }"
-            autocomplete="off"
-        >
-          <a-form-item label="工作大类" name="type1" :rules="[{ required: true, message: '请选择工作大类' }]">
-            <a-select ref="select" v-model:value="logData.type1" style="width: 100%" @change="type1handleChange">
-              <a-select-option v-model:value="item.id" v-for="(item,index) in type1Data" :key="index">
-                {{ item.description }}
-              </a-select-option>
-            </a-select>
-          </a-form-item>
-
-          <a-form-item label="工作子类" name="type2" :rules="[{ required: true, message: '请选择工作子类!' }]">
-            <a-select ref="select" style="width: 100%" v-model:value="logData.type2" @change="type2handleChange">
-              <a-select-option :value="item.id" v-for="(item,index) in type2Data" :key="index">
-                {{ item.description }}
-              </a-select-option>
-            </a-select>
-          </a-form-item>
-        </a-form>
-        <div>
-          <a-textarea :rows="11" v-model:value="logData.content">
-          </a-textarea>
+    <div class="BoxClass">
+        <div class="LeftClass">
+            <div class="CalendarClass">
+                <a-calendar v-model:value="today" :fullscreen="false" valueFormat="YYYY-MM-DD" @select="onSelect"/>
+            </div>
+            <a-button type="primary" style="width: 100%;margin-top: 8px" @click="goToday">今日</a-button>
+            <a-button type="primary" style="width: 100%;margin-top: 8px" @click="GetWeekLog">本周日志</a-button>
         </div>
-        <a-button type="primary" @click="AddLogHandler" style="margin-top: 10px;width: 100%;">添加</a-button>
-      </div>
+        <div class="RightClass">
+            <div v-if="!data.showWeekLog">
+                <a-form
+                        :model="logData"
+                        name="basic"
+                        :label-col="{ span: 8 }"
+                        :wrapper-col="{ span: 16 }"
+                        autocomplete="off"
+                >
+                    <a-form-item label="工作大类" name="type1" :rules="[{ required: true, message: '请选择工作大类' }]">
+                        <a-select ref="select" v-model:value="logData.type1" style="width: 100%"
+                                  @change="type1handleChange">
+                            <a-select-option v-model:value="item.id" v-for="(item,index) in type1Data" :key="index">
+                                {{ item.description }}
+                            </a-select-option>
+                        </a-select>
+                    </a-form-item>
 
-      <div v-if="data.showWeekLog">
-        <div v-for="(item,i) in data.weekData" :key="i">
-          {{ i }}:<br/>
-          <div v-for="(item1,i1) in item" :key="i1">{{ kg }}{{ i1 + 1 }}.{{ item1 }}</div>
+                    <a-form-item label="工作子类" name="type2"
+                                 :rules="[{ required: true, message: '请选择工作子类!' }]">
+                        <a-select ref="select" style="width: 100%" v-model:value="logData.type2"
+                                  @change="type2handleChange">
+                            <a-select-option :value="item.id" v-for="(item,index) in type2Data" :key="index">
+                                {{ item.description }}
+                            </a-select-option>
+                        </a-select>
+                    </a-form-item>
+                </a-form>
+                <div>
+                    <a-textarea :rows="11" v-model:value="logData.content">
+                    </a-textarea>
+                </div>
+                <a-button type="primary" @click="AddLogHandler" style="margin-top: 10px;width: 100%;">添加</a-button>
+            </div>
+
+            <div v-if="data.showWeekLog">
+                <div v-for="(item,i) in data.weekData" :key="i">
+                    {{ i }}:<br/>
+                    <div v-for="(item1,i1) in item" :key="i1">{{ kg }}{{ i1 + 1 }}.{{ item1 }}</div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
 
-  </div>
+    </div>
 
 
 </template>
@@ -69,19 +72,19 @@ const today = ref<Dayjs>();
 
 const logData: IAddLogData = reactive(
     {
-      id: 0,
-      type1: 0,
-      type2: 0,
-      content: "",
-      date: moment(dayjs().format("YYYY-MM-DD")).unix()
+        id: 0,
+        type1: 0,
+        type2: 0,
+        content: "",
+        date: moment(dayjs().format("YYYY-MM-DD")).unix()
     }
 )
 
 
 //  选择日期
 const onSelect = (value: Dayjs, mode: string) => {
-  // 选择日期后 就直接转为 unix 时间戳
-  logData.date = moment(value.toString()).unix()
+    // 选择日期后 就直接转为 unix 时间戳
+    logData.date = moment(value.toString()).unix()
 };
 
 //
@@ -89,8 +92,8 @@ const onSelect = (value: Dayjs, mode: string) => {
 //  @Description: 回到今天
 //
 function goToday() {
-  today.value = dayjs()
-  logData.date = moment(today.value?.format("YYYY-MM-DD")).unix()
+    today.value = dayjs()
+    logData.date = moment(today.value?.format("YYYY-MM-DD")).unix()
 }
 
 //
@@ -98,42 +101,42 @@ function goToday() {
 //  @Description: 获取本周日志
 //
 function GetWeekLog() {
-  data.showWeekLog = !data.showWeekLog
-  if (data.showWeekLog) {
-    FetchWeekLog().then(res => {
-      data.weekData = res.data
-    })
-  }
+    data.showWeekLog = !data.showWeekLog
+    if (data.showWeekLog) {
+        FetchWeekLog().then(res => {
+            data.weekData = res.data
+        })
+    }
 }
 
 const kg = "\u3000\u3000\u3000"
 
 const type1handleChange = async (value: number) => {
 
-  await getType2List({pid: value})
-  logData.type1 = value
-  logData.type2 = type2Data.value[0].id
+    await getType2List({pid: value})
+    logData.type1 = value
+    logData.type2 = type2Data.value[0].id
 };
 
 const type2handleChange = (value: number) => {
-  logData.type2 = value
+    logData.type2 = value
 };
 
 
 let data = reactive({
-  showWeekLog: false,
-  weekData: {},
+    showWeekLog: false,
+    weekData: {},
 });
 
 // 工作大类
 const {type1Data} = storeToRefs(type1Store())
 const {getType1List} = type1Store()
 onMounted(async () => {
-      await getType1List()
+        await getType1List()
 
-      logData.type1 = type1Data.value[0].id
-      await getType2List({pid: logData.type1})
-      logData.type2 = type2Data.value[0].id
+        logData.type1 = type1Data.value[0].id
+        await getType2List({pid: logData.type1})
+        logData.type2 = type2Data.value[0].id
     }
 )
 
@@ -145,16 +148,16 @@ const {getType2List} = type2Store()
 //  添加日志
 const {getLogData} = logStore()
 const AddLogHandler = async () => {
-  if (logData.content === "") {
-    return
-  }
-  logData.content = ""
-  await AddLog(logData)
-  // 添加完毕后要更新数据
-  await getLogData({
-    pageIndex: 1,
-    pageSize: 10
-  })
+    if (logData.content === "") {
+        return
+    }
+    await AddLog(logData)
+    // 添加完毕后要更新数据
+    await getLogData({
+        pageIndex: 1,
+        pageSize: 10
+    })
+    logData.content = ""
 }
 
 
@@ -163,32 +166,32 @@ const AddLogHandler = async () => {
 <style scoped>
 
 .BoxClass {
-  overflow: hidden;
-  width: 100%;
+    overflow: hidden;
+    width: 100%;
 }
 
 .LeftClass {
-  padding: 10px;
-  width: 300px;
-  height: 425px;
-  background: white;
-  float: left;
-  border-radius: 4px;
-  margin-right: 10px;
+    padding: 10px;
+    width: 300px;
+    height: 425px;
+    background: white;
+    float: left;
+    border-radius: 4px;
+    margin-right: 10px;
 }
 
 .CalendarClass {
-  border: 1px solid #F0F2F5;
+    border: 1px solid #F0F2F5;
 }
 
 
 .RightClass {
-  float: left;
-  padding: 10px;
-  width: 300px;
-  background: white;
-  border-radius: 4px;
-  height: 425px;
+    float: left;
+    padding: 10px;
+    width: 300px;
+    background: white;
+    border-radius: 4px;
+    height: 425px;
 }
 
 </style>
